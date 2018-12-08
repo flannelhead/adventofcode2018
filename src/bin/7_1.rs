2edaff87366dc92126aa954e5268b245544a1db2
Sakari Kapanen <sakari.m.kapanen@gmail.com>
2018-12-08 11:46:16 +0200

    Day 7 part 2

--- src/bin/7_1.rs
+++ src/bin/7_1.rs
@@ -2,6 +2,82 @@ use std::fs::File;
 use std::io::prelude::*;
 use std::collections::*;
 
+fn part1(mut steps: HashMap<char, HashSet<char>>) -> () {
+    let mut steps_ordered = String::new();
+    loop {
+        match take_task(&mut steps) {
+            Some(step) => {
+                steps_ordered.push(step);
+                task_done(step, &mut steps);
+            },
+            None => break
+        }
+    }
+
+    println!("{}", steps_ordered);
+}
+
+#[derive(Clone, Debug)]
+struct Task {
+    name: char,
+    finish: i32
+}
+
+fn task_duration(task_name: char) -> i32 {
+    return task_name as i32 - 'A' as i32 + 61;
+}
+
+fn take_task(steps: &mut HashMap<char, HashSet<char>>) -> Option<char> {
+    steps.iter()
+        .filter(|(_, dep_set)| dep_set.is_empty())
+        .min_by_key(|(target, _)| *target)
+        .map(|target| *target.0)
+        .and_then(|name| {
+            steps.remove(&name);
+            Some(name)
+        })
+}
+
+fn task_done(name: char, steps: &mut HashMap<char, HashSet<char>>) {
+    steps.iter_mut().for_each(|(_, dep_set)| { dep_set.remove(&name); });
+}
+
+fn part2(mut steps: HashMap<char, HashSet<char>>) -> () {
+    let mut workers: Vec<Option<Task>> = vec![None; 5];
+    let mut time: i32 = 0;
+    while !steps.is_empty() || workers.iter().any(|worker| worker.is_some()) {
+        println!("{:?}", workers);
+        // Finish the next task
+        let next_work = workers.iter_mut()
+            .filter(|worker| worker.is_some())
+            .min_by_key(|worker| (*worker).clone().unwrap().finish);
+
+        next_work.and_then(|worker: &mut Option<Task>| {
+            match worker {
+                Some(task) => {
+                    time = task.finish;
+                    task_done(task.name, &mut steps);
+                    *worker = None;
+                },
+                None => ()
+            }
+            Some(())
+        });
+
+        // Start new tasks for free workers
+        workers.iter_mut().filter(|worker| worker.is_none())
+            .for_each(|worker| {
+                *worker = take_task(&mut steps)
+                    .map(|name| Task {
+                        name: name,
+                        finish: time + task_duration(name)
+                    });
+            });
+    }
+
+    println!("finish time: {}", time);
+}
+
 fn main() -> std::io::Result<()> {
     let mut datafile = File::open("data/7")?;
     let mut data = String::new();
@@ -14,31 +90,15 @@ fn main() -> std::io::Result<()> {
         })
         .collect();
 
-    let mut deps: HashMap<char, HashSet<char>> = HashMap::new();
+    let mut steps: HashMap<char, HashSet<char>> = HashMap::new();
     for (dep, target) in deps_pairs {
-        deps.entry(target).or_insert(HashSet::new())
+        steps.entry(target).or_insert(HashSet::new())
             .insert(dep);
-        deps.entry(dep).or_insert(HashSet::new());
+        steps.entry(dep).or_insert(HashSet::new());
     }
 
-    let mut steps_ordered = String::new();
-    loop {
-        let chosen_step = deps.iter()
-            .filter(|(_, dep_set)| dep_set.is_empty())
-            .min_by_key(|(target, _)| *target);
-
-        match chosen_step {
-            Some(target) => {
-                let step = *target.0;
-                steps_ordered.push(step);
-                deps.iter_mut().for_each(|(_, dep_set)| { dep_set.remove(&step); });
-                deps.remove(&step);
-            },
-            None => break
-        }
-    }
-
-    println!("{}", steps_ordered);
+    part1(steps.clone());
+    part2(steps.clone());
 
     Ok(())
 }
