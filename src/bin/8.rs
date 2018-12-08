73f8fdb46998336d20f65af830f5e9a71668f05b
Sakari Kapanen <sakari.m.kapanen@gmail.com>
2018-12-08 12:37:48 +0200

    Day 8 part 2

--- src/bin/8.rs
+++ src/bin/8.rs
@@ -25,7 +25,19 @@ fn parse_node<'a>(data: &mut impl Iterator<Item=&'a i32>) -> Node {
 }
 
 fn sum_meta(node: &Node) -> i32 {
-    node.metadata.iter().sum::<i32>() + node.children.iter().map(|node| sum_meta(node)).sum::<i32>()
+    node.metadata.iter().sum::<i32>() +
+        node.children.iter().map(|node| sum_meta(node)).sum::<i32>()
+}
+
+fn value_of_node(node: &Node) -> i32 {
+    if node.children.len() == 0 {
+        return node.metadata.iter().sum()
+    }
+
+    node.metadata.iter().filter_map(|meta| {
+        node.children.get(*meta as usize - 1)
+            .and_then(|node| Some(value_of_node(node)))
+    }).sum()
 }
 
 fn main() -> std::io::Result<()> {
@@ -36,8 +48,9 @@ fn main() -> std::io::Result<()> {
         .filter_map(|x| x.trim().parse().ok())
         .collect();
 
-    let tree = parse_node(&mut numbers.iter());
-    println!("{:?}", sum_meta(&tree));
+    let root_node = parse_node(&mut numbers.iter());
+    println!("{:?}", sum_meta(&root_node));
+    println!("{:?}", value_of_node(&root_node));
 
     Ok(())
 }
